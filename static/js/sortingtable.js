eb47954c394c9b7418c87adb55c23b61522822f4
RandomAnonUser23 <148116540+RandomAnonUser23@users.noreply.github.com>
2023-11-10 12:25:23 +0000

    Add files via upload

--- static/js/sortingtable.js
+++ static/js/sortingtable.js
@@ -1,28 +1,36 @@
 $(document).ready(function () {
-        // Enable sorting on click
-        $('th').click(function () {
-            var table = $('#sortable-table');
-            var index = $(this).index();
+    // Enable sorting on click
+    $('th').click(function () {
+        var table = $('#sortable-table');
+        var index = $(this).index();
+        var rows = table.find('tbody > tr').get();
+        var isAscending = $(this).hasClass('asc');
 
-            // Get the tbody rows and sort them based on the clicked column
-            var rows = table.find('tbody > tr').get().sort(function (a, b) {
+        // Remove asc/desc classes from all headers
+        $('th').removeClass('asc desc');
+
+        // Toggle between ascending and descending
+        if (isAscending) {
+            // Sort in descending order
+            $(this).addClass('desc');
+            rows.sort(function (a, b) {
                 var A = $(a).children('td').eq(index).text().toUpperCase();
                 var B = $(b).children('td').eq(index).text().toUpperCase();
-
-                if (A < B) {
-                    return -1;
-                }
-
-                if (A > B) {
-                    return 1;
-                }
-
-                return 0;
+                return B.localeCompare(A);
             });
-
-            // Append the sorted rows to the table
-            $.each(rows, function (index, row) {
-                table.children('tbody').append(row);
+        } else {
+            // Sort in ascending order
+            $(this).addClass('asc');
+            rows.sort(function (a, b) {
+                var A = $(a).children('td').eq(index).text().toUpperCase();
+                var B = $(b).children('td').eq(index).text().toUpperCase();
+                return A.localeCompare(B);
             });
+        }
+
+        // Append the sorted rows to the table
+        $.each(rows, function (index, row) {
+            table.children('tbody').append(row);
         });
     });
+});
